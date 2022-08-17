1b22441512807271ae9b73c742b47c8b3fb0870f
unknown <marce1994>
2022-08-17 18:16:49 -0300

    services unit testing done

--- services/article.spec.ts
+++ services/article.spec.ts
@@ -1,64 +1,101 @@
-// import { ArticleService } from './article';
-// import * as mockingoose from 'mockingoose';
-// import { Article, Comment } from '../models';
-// import IArticle from '../models/interfaces/IArticle';
+import { ArticleService } from './article';
+import * as mockingoose from 'mockingoose';
+import { Article, Comment } from '../models';
+import IArticle from '../models/interfaces/IArticle';
+import { Query } from 'mongoose';
+import { CommentService } from './comment';
 
-// describe('ArticleService', () => {
-//     it('should return an empty Article array', async () => {
-//         mockingoose(Article).toReturn([] as Array<IArticle>, 'find');
-//         const articles = await ArticleService.fetch();
-//         expect(articles).toEqual(articles);
-//     });
 
-//     // Its not working...
-//     it('should create and return the Article', async () => {
-//         let article = <IArticle>{
-//             title: "Test",
-//             author: "Author",
-//             body: "Body"
-//         };
+let article: IArticle;
+let articleId: string;
 
-//         Article.create = jest.fn().mockReturnValue(Promise.resolve(article));
+describe('ArticleService', () => {
 
-//         const createdArticle = await ArticleService.create(article);
-//         expect(createdArticle).toMatchObject(article); 
-//     });
+    beforeEach(() => {
+        articleId = '62f165797d3272395398efe7';
+        article = <IArticle>{
+            _id: articleId,
+            author: 'author',
+            body: 'body',
+            title: 'title'
+        };
+    });
 
-//     it('should return the Article by id', async () => {
-//         let article = <IArticle>{
-//             title: "Test",
-//             author: "Author",
-//             body: "Body"
-//         };
+    it('GetArticles - WithoutArticles', async function () {
+        mockingoose(Article).toReturn([], 'find');
+        var result = await ArticleService.fetch();
+        expect(result.length).toBe(0);
+    });
 
-//         mockingoose(Article).toReturn(article, 'findOne');
+    it('CreateArticle - OK', async function () {
+        jest.spyOn(Article, 'create').mockImplementation(() => Promise.resolve(article));
+        var result = await ArticleService.create(article);
 
-//         const foundArticle = await ArticleService.findById('5d8f8f8f8f8f8f8f8f8f8f8');
-//         expect(foundArticle).toMatchObject(article);
-//     });
+        expect(result.title).toBe(article.title);
+        expect(result.author).toBe(article.author);
+        expect(result.body).toBe(article.body);
+        expect(result._id.toString()).toBe(articleId);
+    });
 
-//     it('should update the Article by id', async () => {
-//         let article = <IArticle>{
-//             title: "Test",
-//         };
+    it('CreateArticle - and retrieve created', async function () {
+        jest.spyOn(Article, 'create').mockImplementation(() => Promise.resolve(article));
+        let created: any = await ArticleService.create(article);
 
-//         mockingoose(Article).toReturn(article, 'findOneAndUpdate');
-//         const updatedArticle = await ArticleService.update('5d8f8f8f8f8f8f8f8f8f8f8', article);
-//         expect(updatedArticle).toMatchObject(article);
-//     });
+        mockingoose(Article).toReturn(created, 'findOne');
+        var retrieved: any = await ArticleService.findById(created._id);
 
-//     it('should delete the Article by id', async () => {
-//         mockingoose(Comment).toReturn(Promise.resolve(null), 'deleteMany');
-//         mockingoose(Article).toReturn(Promise.resolve(null), 'findByIdAndRemove');
+        expect(created.title).toBe(retrieved.title);
+        expect(created.author).toBe(retrieved.author);
+        expect(created.body).toBe(retrieved.body);
+        expect(created._id.toString()).toBe(retrieved._id.toString());
+    });
 
-//         let spyComment = jest.spyOn(Comment, 'deleteMany');
-//         let spyArticle = jest.spyOn(Article, 'findByIdAndRemove');
+    it('CreateArticle - and retrieve a non existing article', async function () {
+        let articleSpyOn = jest.spyOn(Article, 'create').mockImplementation(() => Promise.resolve(article));
+        await ArticleService.create(article);
 
-//         const deletedArticle = await ArticleService.delete('5d8f8f8f8f8f8f8f8f8f8f8');
+        mockingoose(Article).toReturn(null, 'findOne');
+        var nonExisting = await ArticleService.findById('111165797d3272395398efe7');
 
-//         expect(spyComment).toHaveBeenCalled();
-//         expect(spyArticle).toHaveBeenCalled();
+        expect(articleSpyOn).toHaveBeenCalled();
+        expect(nonExisting).toBe(null);
+    });
 
-//         expect(deletedArticle).toBeUndefined();
-//     });
-// });
+    it('UpdateArticle - validate updated article', async function () {
+        mockingoose(Article).toReturn(article, 'save');
+
+        let created = await ArticleService.create(article);
+        let createdId = created._id.toString();
+
+        article.title = "updated";
+        article.author = "updated";
+        article.body = "updated";
+
+        jest.spyOn(Article, 'findOneAndUpdate').mockImplementation(() => <Query<unknown, unknown, {}, IArticle>>{ exec: () => Promise.resolve(article) });
+        await ArticleService.update(createdId.toString(), article);
+
+        mockingoose(Article).toReturn(article, 'findOne');
+        let updated: any = await ArticleService.findById(createdId);
+
+        expect(article.title).toBe(updated.title);
+        expect(article.author).toBe(updated.author);
+        expect(article.body).toBe(updated.body);
+        expect(createdId.toString()).toBe(updated._id.toString());
+    });
+
+    it('DeleteArticle - OK', async function () {
+        mockingoose(Article).toReturn(article, 'save');
+        await ArticleService.create(article);
+
+        let articleSpyOn = jest.spyOn(Article, 'findByIdAndRemove').mockImplementation(() => <Query<unknown, unknown, {}, IArticle>>{ exec: () => Promise.resolve(article) });
+        let commentDeleteSpyOn = jest.spyOn(CommentService, 'deleteByArticleId').mockImplementation(() => Promise.resolve());
+        await ArticleService.delete(articleId);
+
+        mockingoose(Article).toReturn(null, 'findOne');
+        var nonExisting = await ArticleService.findById(articleId);
+
+        expect(commentDeleteSpyOn).toHaveBeenCalledWith(articleId);
+        expect(articleSpyOn).toHaveBeenCalled();
+        expect(nonExisting).toBeNull();
+    });
+});
--- services/comment.spec.ts
+++ services/comment.spec.ts
@@ -1,93 +1,98 @@
-// import IComment from '../models/interfaces/IComment';
-// import { CommentService } from './comment';
-// import { Types } from 'mongoose';
-// import * as mockingoose from 'mockingoose';
-// import { Comment } from '../models';
+import IComment from '../models/interfaces/IComment';
+import { CommentService } from './comment';
+import { Query, Types } from 'mongoose';
+import * as mockingoose from 'mockingoose';
+import { Comment } from '../models';
 
-// let comment: IComment;
-// let commentId: '62f165797d32723953981111';
-// let articleId: '62f165797d32723953982222';
+let comment: IComment;
+let commentId: string;
+let articleId: string;
 
-// describe('CommentService', () => {
-//     beforeEach(() => {
-//         comment = <IComment>{
-//             _id: '62f165797d32723953981111',
-//             author: 'author',
-//             body: 'body',
-//             articleId: new Types.ObjectId('62f165797d32723953982222')
-//         };
-//         // CommentService.delete(commentId);
-//     });    
+describe('CommentService', () => {
     
-//     it('GetComments - by article empty', async () => {
-//         mockingoose(Comment).toReturn([], 'find');
-//         var result = await CommentService.findByArticleId(articleId);
-//         expect(result.length).toBe(0);
-//     });
+    beforeEach(() => {
+        commentId = '62f165797d32723953981111';
+        articleId = '62f165797d32723953982222';
 
-//     it('CreateComment - OK', async () => {
-//         mockingoose(Comment).toReturn(comment, 'save');
+        comment = <IComment>{
+            _id: new Types.ObjectId(commentId),
+            author: 'author',
+            body: 'body',
+            articleId: new Types.ObjectId(articleId)
+        };
+    });
+
+    it('GetComments - by article empty', async () => {
+        mockingoose(Comment).toReturn([], 'find');
+        var result = await CommentService.findByArticleId(articleId);
+        expect(result.length).toBe(0);
+    });
+
+    it('CreateComment - OK', async () => {
+        jest.spyOn(Comment, 'create').mockImplementation(() => Promise.resolve(comment));
+
+        var result = await CommentService.create(comment);
+
+        expect(result.author).toBe(comment.author);
+        expect(result.body).toBe(comment.body);
+        expect(result._id.toString()).toBe(commentId);
+    });
+
+    it('CreateComment - and retrieve created', async () => {
+        jest.spyOn(Comment, 'create').mockImplementation(() => Promise.resolve(comment));
+        let created: any = await CommentService.create(comment);
+
+        mockingoose(Comment).toReturn(created, 'findOne');
+        var retrieved: any = await CommentService.findById(created._id);
+
+        expect(created.author).toBe(retrieved.author);
+        expect(created.body).toBe(retrieved.body);
+        expect(created._id.toString()).toBe(retrieved._id.toString());
+    });
+
+    it('CreateComment - and retrieve a non existing comment', async () => {
+        let commentSpyOn = jest.spyOn(Comment, 'create').mockImplementation(() => Promise.resolve(comment));
+        await CommentService.create(comment);
+
+        mockingoose(Comment).toReturn(null, 'findOne');
+        var nonExisting = await CommentService.findById('111165797d3272395398efe7');
+
+        expect(commentSpyOn).toHaveBeenCalled();
+        expect(nonExisting).toBeNull();
+    });
+
+    it('UpdateComment - validate updated comment', async () => {
+        mockingoose(Comment).toReturn(comment, 'save');
         
-//         var result = await CommentService.create(comment);
+        let created = await CommentService.create(comment);
+        let createdId = created._id.toString();
+        
+        comment.author = "updated";
+        comment.body = "updated";
 
-//         expect(result.author).toBe(comment.author);
-//         expect(result.body).toBe(comment.body);
-//         expect(result._id.toString()).toBe(commentId.toString());
-//     });
+        let commentSpyOn = jest.spyOn(Comment, 'findOneAndUpdate').mockImplementation(() => <Query<unknown,unknown,{},IComment>>{ exec: () => Promise.resolve(comment) });
+        await CommentService.update(createdId, comment);
 
-//     it('CreateComment - and retrieve created', async () => {
-//         mockingoose(Comment).toReturn(comment, 'save');
-//         let created: any = await CommentService.create(comment);
+        mockingoose(Comment).toReturn(comment, 'findOne');
+        let updated: any = await CommentService.findById(createdId);
 
-//         mockingoose(Comment).toReturn(created, 'findOne');
-//         var retrieved: any = await CommentService.findById(created._id);
+        expect(commentSpyOn).toHaveBeenCalled();
+        expect(comment.author).toBe(updated.author);
+        expect(comment.body).toBe(updated.body);
+        expect(createdId).toBe(updated._id.toString());
+    });
 
-//         expect(created.author).toBe(retrieved?.author);
-//         expect(created.body).toBe(retrieved?.body);
-//         expect(created._id.toString()).toBe(retrieved?._id.toString());
-//     });
+    it('DeleteComment - OK', async () => {
+        mockingoose(Comment).toReturn(comment, 'findOneAndRemove');
+        let created = await CommentService.create(comment);
+        let createdId = created._id.toString();
 
-//     it('CreateComment - and retrieve a non existing comment', async () => {
-//         let commentSpyOn = jest.spyOn(Comment, 'findOne').mockReturnValue(null);
+        let commentSpyOn = jest.spyOn(Comment, 'findOneAndRemove').mockImplementation(() => <Query<unknown,unknown,{},IComment>>{ exec: () => Promise.resolve(comment) });
+        await CommentService.delete(createdId);
+        
+        mockingoose(Comment).toReturn(null, 'findOne');
+        var nonExisting = await CommentService.findById(commentId);
 
-//         await CommentService.create(comment);
-
-//         mockingoose(Comment).toReturn(null, 'findOne');
-//         var nonExisting = await CommentService.findById('111165797d3272395398efe7');
-
-//         expect(commentSpyOn).toHaveBeenCalled();
-//         expect(nonExisting).toBeNull();
-//     });
-
-//     it('UpdateComment - validate updated comment', async () => {
-//         mockingoose(Comment).toReturn(comment, 'save');
-
-//         let created = await CommentService.create(comment);
-//         let createdId = created._id.toString();
-//         comment.author = "updated";
-//         comment.body = "updated";
-
-//         mockingoose(Comment).toReturn(comment, 'findOneAndUpdate');
-//         await CommentService.update(createdId, comment);
-
-//         mockingoose(Comment).toReturn(comment, 'findOne');
-//         let updated: any = await CommentService.findById(createdId); 
-
-//         expect(comment.author).toBe(updated.author);
-//         expect(comment.body).toBe(updated.body);
-//         expect(createdId).toBe(updated._id.toString());
-//     });
-
-//     it('DeleteComment - OK', async () => {
-//         mockingoose(Comment).toReturn(comment, 'save');
-//         await CommentService.create(comment);
-
-//         mockingoose(Comment).toReturn(null, 'findOneAndRemove');
-//         await CommentService.delete(commentId);
-
-//         var nonExisting = await CommentService.findById(commentId);
-
-//         expect(nonExisting).toBeNull();
-//     });
-
-// });
+        expect(nonExisting).toBeNull();
+    });
+});
